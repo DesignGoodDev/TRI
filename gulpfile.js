0985ec3f4532ceff7b3b5195d005523403f04178
Leslie Crosby <leslie@DesignGoods-iMac-Leslie.local>
2019-11-01 16:05:54 -0500

    Switched to gulp boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,295 @@
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    sass = require('gulp-sass'),
-    cssnano = require('gulp-cssnano'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sourcemaps = require('gulp-sourcemaps'),
-    jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    plumber = require('gulp-plumber'),
-    bower = require('gulp-bower'),
-    babel = require('gulp-babel'),
-    livereload = require('gulp-livereload')
-    // browserSync = require('browser-sync').create();
+/**
+ * Settings
+ * Turn on/off build features
+ */
 
-    // JSHint, concat, and minify JavaScript
-    gulp.task('site-js', function() {
-      return gulp.src([
-
-               // Grab your custom scripts
-      		  './assets/js/scripts/*.js'
-
-      ])
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(concat('scripts.js'))
-        .pipe(gulp.dest('./assets/js'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.')) // Creates sourcemap for minified JS
-        .pipe(gulp.dest('./assets/js'))
-    });
-
-    // Compile Sass, Autoprefix and minify
-    gulp.task('styles', function() {
-        return gulp.src('./assets/scss/**/*.scss')
-            .pipe(plumber(function(error) {
-                gutil.log(gutil.colors.red(error.message));
-                this.emit('end');
-            }))
-            .pipe(sourcemaps.init()) // Start Sourcemaps
-            .pipe(sass())
-            .pipe(autoprefixer({
-                browsers: ['last 2 versions'],
-                cascade: false
-            }))
-            .pipe(gulp.dest('./assets/css/'))
-            .pipe(rename({suffix: '.min'}))
-            .pipe(cssnano())
-            .pipe(sourcemaps.write('.')) // Creates sourcemaps for minified styles
-            .pipe(gulp.dest('./assets/css/'))
-            .pipe(livereload());
-    });
-
-    // Browser-Sync watch files and inject changes
-    // gulp.task('browsersync', function() {
-    //     // Watch files
-    //     var files = [
-    //     	'./assets/css/*.css',
-    //     	'./assets/js/*.js',
-    //     	'**/*.html',
-    //     	'assets/images/**/*.{png,jpg,gif,svg,webp}',
-    //     ];
-    //
-    //     browserSync.init(files, {
-    // 	    // Replace with URL of your local site
-    // 	    // proxy: "http://localhost/",
-    //       proxy: "file:///Users/leslierobertson/Sites/Static/TRI/index.html"
-    //     });
-    //
-    //     gulp.watch('./assets/scss/**/*.scss', ['styles']);
-    //     gulp.watch('./assets/js/scripts/*.js', ['site-js']).on('change', browserSync.reload);
-    //
-    // });
-
-    // Watch files for changes (without Browser-Sync)
-    gulp.task('watch', function() {
-
-      var server = livereload();
-
-      // Watch .scss files
-      gulp.watch('./assets/scss/**/*.scss', ['styles']);
-
-      // Watch site-js files
-      gulp.watch('./assets/js/scripts/*.js', ['site-js']);
+var settings = {
+	clean: true,
+	scripts: true,
+	polyfills: true,
+	styles: true,
+	svgs: false,
+	copy: true,
+	reload: true
+};
 
 
-    });
+/**
+ * Paths to project folders
+ */
 
-    // Run styles, site-js and foundation-js
-    gulp.task('default', function() {
-      gulp.start('styles', 'site-js');
-    });
+var paths = {
+	input: 'src/',
+	output: 'dist/',
+	scripts: {
+		input: 'src/js/*',
+		polyfills: '.polyfill.js',
+		output: 'dist/js/'
+	},
+	styles: {
+		input: 'src/sass/**/*.{scss,sass}',
+		output: 'dist/css/'
+	},
+	svgs: {
+		input: 'src/svg/*.svg',
+		output: 'dist/svg/'
+	},
+	copy: {
+		input: 'src/copy/**/*',
+		output: 'dist/'
+	},
+	reload: './dist/'
+};
+
+
+/**
+ * Template for banner to add to file headers
+ */
+
+var banner = {
+	main:
+		'/*!' +
+		' <%= package.name %> v<%= package.version %>' +
+		' | (c) ' + new Date().getFullYear() + ' <%= package.author.name %>' +
+		' | <%= package.license %> License' +
+		' | <%= package.repository.url %>' +
+		' */\n'
+};
+
+
+/**
+ * Gulp Packages
+ */
+
+// General
+var {gulp, src, dest, watch, series, parallel} = require('gulp');
+var del = require('del');
+var flatmap = require('gulp-flatmap');
+var lazypipe = require('lazypipe');
+var rename = require('gulp-rename');
+var header = require('gulp-header');
+var package = require('./package.json');
+
+// Scripts
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var concat = require('gulp-concat');
+var uglify = require('gulp-terser');
+var optimizejs = require('gulp-optimize-js');
+
+// Styles
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var prefix = require('autoprefixer');
+var minify = require('cssnano');
+
+// SVGs
+var svgmin = require('gulp-svgmin');
+
+// BrowserSync
+var browserSync = require('browser-sync');
+
+
+/**
+ * Gulp Tasks
+ */
+
+// Remove pre-existing content from output folders
+var cleanDist = function (done) {
+
+	// Make sure this feature is activated before running
+	if (!settings.clean) return done();
+
+	// Clean the dist folder
+	del.sync([
+		paths.output
+	]);
+
+	// Signal completion
+	return done();
+
+};
+
+// Repeated JavaScript tasks
+var jsTasks = lazypipe()
+	.pipe(header, banner.main, {package: package})
+	.pipe(optimizejs)
+	.pipe(dest, paths.scripts.output)
+	.pipe(rename, {suffix: '.min'})
+	.pipe(uglify)
+	.pipe(optimizejs)
+	.pipe(header, banner.main, {package: package})
+	.pipe(dest, paths.scripts.output);
+
+// Lint, minify, and concatenate scripts
+var buildScripts = function (done) {
+
+	// Make sure this feature is activated before running
+	if (!settings.scripts) return done();
+
+	// Run tasks on script files
+	return src(paths.scripts.input)
+		.pipe(flatmap(function(stream, file) {
+
+			// If the file is a directory
+			if (file.isDirectory()) {
+
+				// Setup a suffix variable
+				var suffix = '';
+
+				// If separate polyfill files enabled
+				if (settings.polyfills) {
+
+					// Update the suffix
+					suffix = '.polyfills';
+
+					// Grab files that aren't polyfills, concatenate them, and process them
+					src([file.path + '/*.js', '!' + file.path + '/*' + paths.scripts.polyfills])
+						.pipe(concat(file.relative + '.js'))
+						.pipe(jsTasks());
+
+				}
+
+				// Grab all files and concatenate them
+				// If separate polyfills enabled, this will have .polyfills in the filename
+				src(file.path + '/*.js')
+					.pipe(concat(file.relative + suffix + '.js'))
+					.pipe(jsTasks());
+
+				return stream;
+
+			}
+
+			// Otherwise, process the file
+			return stream.pipe(jsTasks());
+
+		}));
+
+};
+
+// Lint scripts
+var lintScripts = function (done) {
+
+	// Make sure this feature is activated before running
+	if (!settings.scripts) return done();
+
+	// Lint scripts
+	return src(paths.scripts.input)
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'));
+
+};
+
+// Process, lint, and minify Sass files
+var buildStyles = function (done) {
+
+	// Make sure this feature is activated before running
+	if (!settings.styles) return done();
+
+	// Run tasks on all Sass files
+	return src(paths.styles.input)
+		.pipe(sass({
+			outputStyle: 'expanded',
+			sourceComments: true
+		}))
+		.pipe(postcss([
+			prefix({
+				cascade: true,
+				remove: true
+			})
+		]))
+		.pipe(header(banner.main, {package: package}))
+		.pipe(dest(paths.styles.output))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(postcss([
+			minify({
+				discardComments: {
+					removeAll: true
+				}
+			})
+		]))
+		.pipe(dest(paths.styles.output));
+
+};
+
+// Optimize SVG files
+var buildSVGs = function (done) {
+
+	// Make sure this feature is activated before running
+	if (!settings.svgs) return done();
+
+	// Optimize SVG files
+	return src(paths.svgs.input)
+		.pipe(svgmin())
+		.pipe(dest(paths.svgs.output));
+
+};
+
+// Copy static files into output folder
+var copyFiles = function (done) {
+
+	// Make sure this feature is activated before running
+	if (!settings.copy) return done();
+
+	// Copy static files
+	return src(paths.copy.input)
+		.pipe(dest(paths.copy.output));
+
+};
+
+// Watch for changes to the src directory
+var startServer = function (done) {
+
+	// Make sure this feature is activated before running
+	if (!settings.reload) return done();
+
+	// Initialize BrowserSync
+	browserSync.init({
+		server: {
+			baseDir: paths.reload
+		}
+	});
+
+	// Signal completion
+	done();
+
+};
+
+// Reload the browser when files change
+var reloadBrowser = function (done) {
+	if (!settings.reload) return done();
+	browserSync.reload();
+	done();
+};
+
+// Watch for changes
+var watchSource = function (done) {
+	watch(paths.input, series(exports.default, reloadBrowser));
+	done();
+};
+
+
+/**
+ * Export Tasks
+ */
+
+// Default task
+// gulp
+exports.default = series(
+	cleanDist,
+	parallel(
+		buildScripts,
+		lintScripts,
+		buildStyles,
+		buildSVGs,
+		copyFiles
+	)
+);
+
+// Watch and reload
+// gulp watch
+exports.watch = series(
+	exports.default,
+	startServer,
+	watchSource
+);
---
+++ src/js/scripts/scripts.js
@@ -0,0 +1,238 @@
+// Init controller
+var controller = new ScrollMagic.Controller();
+
+var blockTween2 = new TweenMax.to('#section-2-text', 3.5, {
+  paddingLeft: '15%'
+});
+var blockTween3 = new TweenMax.to('#section-3-text', 3.5, {
+  paddingLeft: '15%'
+});
+var blockTween4 = new TweenMax.to('#section-4-text', 3.5, {
+  paddingLeft: '15%'
+});
+var blockTween5 = new TweenMax.to('#section-5-text', 3.5, {
+  paddingLeft: '15%'
+});
+
+
+var blockTween2img = new TweenMax.to('#section-2-img', 3.5, {
+  top: '50%',
+  width: "298px"
+});
+var blockTween3img = new TweenMax.to('#section-3-img', 3.5, {
+  right: "-60%",
+  width: "244px"
+});
+var blockTween4img = new TweenMax.to('#section-4-img', 3.5, {
+  top: '50%',
+  width: "225px"
+});
+var blockTween5img = new TweenMax.to('#section-5-img', 3.5, {
+  right: '-60%',
+  width: "253px"
+});
+
+
+// build scenes F O R  P A R A L L A X
+new ScrollMagic.Scene({
+  triggerElement: "#section-2",
+  triggerHook: "onEnter",
+  duration: "200%"
+})
+.setTween("#section-2 > #section-2-inner", {y: "20%", ease: Linear.easeNone})
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#section-3",
+  triggerHook: "onEnter",
+  duration: "200%"
+})
+.setTween("#section-3 > #section-3-inner", {y: "20%", ease: Linear.easeNone})
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#section-4",
+  triggerHook: "onEnter",
+  duration: "200%"
+})
+.setTween("#section-4 > #section-4-inner", {y: "20%", ease: Linear.easeNone})
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#section-5",
+  triggerHook: "onEnter",
+  duration: "150%"
+})
+.setTween("#section-5 > #section-5-inner", {y: "20%", ease: Linear.easeNone})
+.addTo(controller);
+
+// build scenes F O R  A N I M A T I O N S
+new ScrollMagic.Scene({
+  triggerElement: "#section-2",
+  triggerHook: "onEnter",
+  duration: "100%"
+})
+.setTween(blockTween2)
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#section-3",
+  triggerHook: "onEnter",
+  duration: "100%"
+})
+.setTween(blockTween3)
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#section-4",
+  triggerHook: "onEnter",
+  duration: "100%"
+})
+.setTween(blockTween4)
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#section-5",
+  triggerHook: "onEnter",
+  duration: "100%"
+})
+.setTween(blockTween5)
+.addTo(controller);
+
+
+new ScrollMagic.Scene({
+  triggerElement: "#section-2",
+  triggerHook: "onEnter",
+  duration: "90%",
+  offset: -50
+})
+.setTween(blockTween2img)
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#section-3",
+  triggerHook: "onEnter",
+  duration: "90%",
+  offset: -50
+})
+.setTween(blockTween3img)
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#section-4",
+  triggerHook: "onEnter",
+  duration: "90%",
+  offset: -50
+})
+.setTween(blockTween4img)
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#section-5",
+  triggerHook: "onEnter",
+  duration: "90%",
+  offset: -50
+})
+.setTween(blockTween5img)
+.addTo(controller);
+
+
+var scenes = {
+'scene1': {
+  'section-1': 'anchor1'
+},
+'scene2': {
+  'section-2': 'anchor2'
+},
+'scene3': {
+  'section-3': 'anchor3'
+},
+'scene4': {
+  'section-4': 'anchor4'
+},
+'scene5': {
+  'section-5': 'anchor5'
+}
+}
+
+for(var key in scenes) {
+// skip loop if the property is from prototype
+if (!scenes.hasOwnProperty(key)) continue;
+
+var obj = scenes[key];
+
+for (var prop in obj) {
+  // skip loop if the property is from prototype
+  if(!obj.hasOwnProperty(prop)) continue;
+
+  new ScrollMagic.Scene({
+    triggerElement: '#' + prop,
+    duration: $('section').height(),
+    triggerHook: .025,
+    reverse: true
+  })
+  .setClassToggle('#' + obj[prop], 'active')
+  // .addIndicators()
+  .addTo(controller);
+}
+}
+
+// Change behaviour of controller
+// to animate scroll instead of jump
+controller.scrollTo(function(target) {
+
+TweenMax.to(window, 0.5, {
+  scrollTo : {
+    y : target,
+    autoKill : true // Allow scroll position to change outside itself
+  },
+  ease : Cubic.easeInOut
+});
+
+});
+
+//  Bind scroll to anchor links using Vanilla JavaScript
+var anchor_nav = document.querySelector('.anchor-nav');
+
+anchor_nav.addEventListener('click', function(e) {
+var target = e.target,
+    id     = target.getAttribute('href');
+
+if(id !== null) {
+  if(id.length > 0) {
+    e.preventDefault();
+    controller.scrollTo(id);
+
+    if(window.history && window.history.pushState) {
+      history.pushState("", document.title, id);
+    }
+  }
+}
+});
+
+/* Bind scroll to anchor links using jQuery */
+
+$(document).on("click", "a[href^=\\#]", function(e) {
+var id = $(this).attr("href");
+
+if($(id).length > 0) {
+  e.preventDefault();
+
+  // trigger scroll
+  controller.scrollTo(id);
+
+  // If supported by the browser we can also update the URL
+  if (window.history && window.history.pushState) {
+    history.pushState("", document.title, id);
+  }
+}
+});
+
+// makes "overlap from bottom effect"
+new ScrollMagic.Scene ({
+  duration: 100,
+  offset: 50
+})
+.setPin("#section-6")
+// .addIndicators()
+.addTo(controller);
